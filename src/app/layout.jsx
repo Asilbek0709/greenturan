72945ea9e588bffefc57d6b3a98960f08afcec10
Asilbek0709 <tasilbek348@gmail.com>
2026-01-09 10:21:22 +0500

    Fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,18 @@
 import "./globals.css";
 import I18nProvider from "./components/I18nProvider";
 
+import { Open_Sans } from 'next/font/google'
+
+const openSans = Open_Sans({
+  subsets: ['latin'],      // поддерживаемые символы
+  weight: ['400', '600', '700'],  // Regular, Medium, Bold
+  display: 'swap',
+})
+
 export default function RootLayout({ children }) {
   return (
     <html lang="uz">
-      <body>
+      <body className={openSans.className}>
         <I18nProvider>
           {children}
         </I18nProvider>
